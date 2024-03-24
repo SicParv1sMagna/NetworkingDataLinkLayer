fbca67791c4e64dd39e9bd7ba3e221a637c345aa
Mark Grevtsov <markgrevtsov2310@gmail.com>
2024-03-24 23:19:38 +0300

    fix

--- internal/segment/model.go
+++ internal/segment/model.go
@@ -7,5 +7,5 @@ type Segment struct {
 	TotalSegments uint      `json:"total_segments" example:"10"`
 	SenderName    string    `json:"sender_name" example:"Марк Гревцов"`
 	SegmentNumber uint      `json:"segment_number" example:"1"`
-	Payload []byte `json:"payload" example:"116,104,105,115,32,105,115,32,97,32,98,121,116,101,32,97,114,114,97,121,32,111,102,32,98,121,116,101,115"`
+	Payload []byte `json:"payload" example:"116,104,105,115,32,105,115,32,97,32"`
 }
